a2f51ce620715ca2e7c31a4e00d55e55573bcb25
pasmophobia <l@leonmatsuistroemsvik.com>
2025-01-20 17:19:53 +0900

    feat: add FlowHandler

--- src/main/kotlin/jp/trap/conqest/Main.kt
+++ src/main/kotlin/jp/trap/conqest/Main.kt
@@ -1,5 +1,6 @@
 package jp.trap.conqest
 
+import jp.trap.conqest.util.FlowHandler
 import org.bukkit.plugin.java.JavaPlugin
 
 const val figlet = """
@@ -11,11 +12,22 @@ const val figlet = """
     """
 
 class Main: JavaPlugin() {
-    override fun onEnable() {
+    private lateinit var flowHandler: FlowHandler
+
+    override fun onLoad() {
         logger.info(figlet)
+        flowHandler = FlowHandler(
+            logger,
+            listOf()
+        )
+    }
+
+    override fun onEnable() {
+        flowHandler.up()
     }
 
     override fun onDisable() {
-
+        flowHandler.down()
+        logger.info("bye bye.")
     }
 }
---
+++ src/main/kotlin/jp/trap/conqest/util/FlowHandler.kt
@@ -0,0 +1,38 @@
+package jp.trap.conqest.util
+
+import java.util.logging.Logger
+
+class FlowHandler(private val logger: Logger, private val tasks: Collection<FlowTask>) {
+    fun up(): Result<Unit> {
+        tasks.forEachIndexed { index, task ->
+            logger.info("Initializing task #${index}")
+            task.up().onSuccess {
+                logger.info("Initialized task #${index}")
+            }.onFailure {
+                logger.severe("Error initializing task #${index}")
+                it.printStackTrace()
+                return Result.failure(it)
+            }
+        }
+        return Result.success(Unit)
+    }
+
+    fun down(): Result<Unit> {
+        tasks.forEachIndexed { index, task ->
+            logger.info("Finalizing task #${index}")
+            task.down().onSuccess {
+                logger.info("Finalizing task #${index}")
+            }.onFailure {
+                logger.severe("Error finalizing task #${index}")
+                it.printStackTrace()
+                return Result.failure(it)
+            }
+        }
+        return Result.success(Unit)
+    }
+}
+
+class FlowTask(
+    val up: () -> Result<Unit>,
+    val down: () -> Result<Unit>,
+)
