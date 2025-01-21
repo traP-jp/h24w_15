1b1749ed184f7e624804bbf3b86231ef0694bb17
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-01-21 23:16:36 +0900

    move GameCommand enum

--- src/main/kotlin/jp/trap/conqest/game/GameState.kt
+++ src/main/kotlin/jp/trap/conqest/game/GameState.kt
@@ -2,10 +2,6 @@ package jp.trap.conqest.game
 
 import org.bukkit.command.CommandSender
 
-enum class GameCommand {
-    JOIN,
-}
-
 abstract class GameState(private val gameManager: GameManager) {
     open fun executeCommand(command: GameCommand, sender: CommandSender): Int {
         sender.sendMessage("このコマンドは現在実行できません。")
