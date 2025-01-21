db8f46ef70c6e4b38854af340da7be6a330b69e4
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-01-21 18:19:03 +0900

    summon nite / follow and attack to player

--- src/main/kotlin/jp/trap/conqest/commands/CommandConQest.kt
+++ src/main/kotlin/jp/trap/conqest/commands/CommandConQest.kt
@@ -1,9 +1,15 @@
 package jp.trap.conqest.commands
 
+import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.tree.LiteralCommandNode
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import jp.trap.conqest.Main
+import jp.trap.conqest.game.nite.NormalNite
 import net.kyori.adventure.text.Component
+import org.bukkit.entity.*
+
+// TODO 仮 本体はNiteManager的なところに保持するべき
+var nite: NormalNite? = null
 
 class CommandConQest(val plugin: Main) : Commands.Command {
     override fun literalCommandNode(): LiteralCommandNode<CommandSourceStack> {
@@ -15,6 +21,19 @@ class CommandConQest(val plugin: Main) : Commands.Command {
                 )
                 0
             }
+            .then(
+                io.papermc.paper.command.brigadier.Commands.literal("summon_nite").then(
+                    io.papermc.paper.command.brigadier.Commands.argument("name", StringArgumentType.string())
+                        .executes { ctx ->
+                            if ((ctx.source.sender is Player).not()) return@executes 0
+                            val player = ctx.source.sender as Player
+                            val arg = ctx.getArgument("name", String::class.java)
+                            // TODO: 仮 本来はNiteManager的なところに保持するべき
+                            nite = NormalNite(player.location, arg, plugin)
+                            0
+                        }
+                )
+            )
             .build()
     }
 
---
+++ src/main/kotlin/jp/trap/conqest/game/Nite.kt
@@ -0,0 +1,44 @@
+package jp.trap.conqest.game
+
+import net.kyori.adventure.text.Component
+import org.bukkit.Location
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.Entity
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Mob
+import org.bukkit.plugin.Plugin
+
+abstract class Nite<T>(location: Location, type: EntityType, name: String, plugin: Plugin)
+        where T : Entity, T : Mob {
+    private var entity: T = location.world.spawnEntity(
+        location, type, false
+    ) as T
+    private var target: LivingEntity? = null
+    private var attack: Boolean = true
+    private val speed = 0.5
+    private val damage = 1.0
+
+    init {
+        target?.sendMessage("aaaa")
+        entity.customName(Component.text(name))
+        if (entity.getAttribute(Attribute.ATTACK_DAMAGE) == null) {
+            entity.registerAttribute(Attribute.ATTACK_DAMAGE)
+            entity.getAttribute(Attribute.ATTACK_DAMAGE)?.baseValue = damage
+        }
+        plugin.server.scheduler.runTaskLater(plugin, Runnable { update(plugin) }, 1)
+    }
+
+    private fun update(plugin: Plugin) {
+        if (target != null) {
+            entity.pathfinder.moveTo(target!!, speed)
+            if (attack && entity.location.distance(target!!.location) <= 3) {
+                entity.attack(target!!)
+            }
+        } else {
+            entity.pathfinder.stopPathfinding()
+        }
+        plugin.server.scheduler.runTaskLater(plugin, Runnable { update(plugin) }, 1)
+    }
+
+}
