73473bb994d3fcfc7a812c7a2f8d081bd475870b
eoate <tsubasa20060117@icloud.com>
2025-01-23 14:50:23 +0900

    delete some from Listeners.kt

--- src/main/kotlin/jp/trap/conqest/listeners/Listeners.kt
+++ src/main/kotlin/jp/trap/conqest/listeners/Listeners.kt
@@ -1,22 +1,16 @@
 package jp.trap.conqest.listeners
 
 import jp.trap.conqest.Main
-import jp.trap.conqest.game.ChanceCard
 import org.bukkit.Bukkit
-import org.bukkit.ChatColor
-import org.bukkit.Material
-import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerInteractEvent
 
 class Listeners(
     private val plugin: Main
-) :Listener{
+){
     private fun registerListener(listener: Listener) {
         Bukkit.getPluginManager().registerEvents(listener, plugin)
     }
     fun init(): Result<Unit> {
-        registerListener(this)
         registerListener(ListenerPlayerUseChanceCard())
         return Result.success(Unit)
     }
