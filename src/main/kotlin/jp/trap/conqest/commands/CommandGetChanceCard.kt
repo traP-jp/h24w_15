fb58187af5ec2e1452bc49e570b33d2c5cdd63b7
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-01-24 16:33:58 +0900

    fix command handler to support UsableItem

--- src/main/kotlin/jp/trap/conqest/commands/CommandGetChanceCard.kt
+++ src/main/kotlin/jp/trap/conqest/commands/CommandGetChanceCard.kt
@@ -4,22 +4,22 @@ import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.tree.LiteralCommandNode
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import jp.trap.conqest.game.item.ChanceCard
+import jp.trap.conqest.listeners.ListenerUsableItem
 import org.bukkit.entity.Player
 
 class CommandGetChanceCard : Commands.Command {
     override fun literalCommandNode(): LiteralCommandNode<CommandSourceStack> {
-        return io.papermc.paper.command.brigadier.Commands
-            .literal("getchancecard")
-            .executes(::executeGetChanceCard)
+        return io.papermc.paper.command.brigadier.Commands.literal("getchancecard").executes(::executeGetChanceCard)
             .build()
     }
 
     private fun executeGetChanceCard(context: CommandContext<CommandSourceStack>): Int {
         val source = context.source
-        if(source.sender is Player){
-            var player = source.sender as Player
+        if (source.sender is Player) {
+            val player = source.sender as Player
+            ListenerUsableItem.registry.plus(ChanceCard)
             player.inventory.addItem(ChanceCard.item)
-        }else {
+        } else {
             source.sender.sendMessage("このコマンドはプレイヤーのみ使用できます")
         }
         return 1
