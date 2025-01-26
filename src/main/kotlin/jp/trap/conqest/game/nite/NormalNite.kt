71451543de2def5938be463e23f67eecf6c2059e
eoate <tsubasa20060117@icloud.com>
2025-01-26 13:04:58 +0900

    change to pillager

--- src/main/kotlin/jp/trap/conqest/game/nite/NormalNite.kt
+++ src/main/kotlin/jp/trap/conqest/game/nite/NormalNite.kt
@@ -3,12 +3,12 @@ package jp.trap.conqest.game.nite
 import jp.trap.conqest.game.Nite
 import org.bukkit.Location
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.Pillager
 import org.bukkit.entity.Player
-import org.bukkit.entity.Villager
 import org.bukkit.plugin.Plugin
 
 class NormalNite(location: Location, override val name: String = "ノーマル", master: Player, plugin: Plugin) :
-    Nite<Villager>(location, EntityType.VILLAGER, name, master, plugin) {
+    Nite<Pillager>(location, EntityType.PILLAGER, name, master, plugin) {
     override val speed = 0.5
     override val damage = 1.0
     override val handLength = 3.0
